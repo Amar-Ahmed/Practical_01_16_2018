5c159cea022df58f7b45ffd3c767c145e43032ff
Amar Ahmed <amar.ahmed64@qmail.cuny.edu>
2018-01-16 21:58:42 -0500

    Update LoopActivity.java

--- app/src/main/java/com/example/android/loopactivity/LoopActivity.java
+++ app/src/main/java/com/example/android/loopactivity/LoopActivity.java
@@ -2,7 +2,8 @@ package com.example.android.loopactivity;
 
 import android.content.Intent;
 import android.os.AsyncTask;
-
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class LoopActivity extends AppCompatActivity {
