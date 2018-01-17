04e333d0b8bac5137b5e7c95cd044dc861e0e5bd
Amar Ahmed <amar.ahmed64@qmail.cuny.edu>
2018-01-16 22:06:15 -0500

    Update ListActivity.java

--- app/src/main/java/com/example/android/loopactivity/ListActivity.java
+++ app/src/main/java/com/example/android/loopactivity/ListActivity.java
@@ -1,17 +1,29 @@
 package com.example.android.loopactivity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by C4Q on 1/16/18.
  */
 
-public class ListActivity extends AppCompatActivity{
-
+public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        RecyclerViewFragment recyclerViewFragment = new RecyclerViewFragment();
+        FragmentManager fragmentMananger = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentMananger.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container,recyclerViewFragment);
+        fragmentTransaction.commit();
+
     }
+
+
+
 }
